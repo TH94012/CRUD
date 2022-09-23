d5d8cafd7ed39669f551367fab920b04f1a51b76
Thiago Costa Thimoteo <thimoteocostathiago@gmail.com>
2022-09-23 17:34:24 -0300

    Enviei o projeto no git

---
+++ main.py
@@ -0,0 +1,80 @@
+from system import *
+
+
+localization = 'c:/Users/User/dev/CRUD/'
+database_name = 'database.json'
+
+create_database(localization, database_name)
+data = get_data(localization, database_name)
+
+logged = False
+
+
+while True:
+    header('Bem vindo ao Login')
+    print('''\033[1;33m
+1 - Login
+2 - Registrar
+3 - Sair do programa
+\033[m''')
+    while True:
+        option1 = int(input(cyan + 'Opção: ' + yellow))
+        if option1 > 3:
+            continue
+        elif option1 < 1:
+            continue
+        break
+    if option1 == 1:
+        username_logged, email_logged, password_logged, rank_logged, victories_logged, defeats_logged, games_logged, id_logged = login(localization, database_name)
+        logged = True
+        #print(F'{username_logged} {email_logged} {password_logged} {rank_logged} {victories_logged} {id_logged}')
+        while True:
+            if logged == False:
+                break
+            header(F'Conectado com o rank {rank_logged}')
+            print('''
+1 - Forca
+2 - Logout
+3 - Informações Conta
+''')
+            if rank_logged == 'admin':
+                print('''4 - Gerenciar contas
+''')
+            while True:
+                try:
+                    option2 = int(input('Opção: '))
+                    if 0 < option2 < 5:
+                        if option2 == 4:
+                            if rank_logged == 'admin':
+                                pass
+                            else:
+                                continue
+                except:
+                    continue
+                else:
+                    break
+            match option2:
+                case 1:
+                    forca(localization, database_name, get_index_by_id(localization, database_name, id_logged))
+                case 2:
+                    username_logged = email_logged = password_logged = rank_logged = victories_logged = id_logged = None
+                    logged = False
+                    break
+                case 3:
+                    while True:
+                        info(username_logged, email_logged, password_logged, rank_logged, victories_logged, defeats_logged, games_logged, id_logged)
+                        while True:
+                            voltar = str(input('Voltar? [S/N] ')).strip().upper()
+                            if voltar == 'S':
+                                break
+                        break
+                case 4:
+                    #funcao management
+                    pass
+                case _:
+                    raise Exception('Você bugou o sistema!')
+    elif option1 == 2:
+        register(localization, database_name)
+    else:
+        break
+print(F'{cyan}Volte Sempre!\033[m')
---
+++ system.py
@@ -0,0 +1,175 @@
+import json, string, random, forca
+from time import sleep
+
+
+cyan = '\033[1;36m'
+yellow = '\033[1;33m'
+
+def create_database(localization, database):
+    try:
+        with open(localization + database) as data:
+            pass
+    except:
+        with open(localization + database, 'at+') as data:
+            data.write('[]')
+
+def get_data(localization, database_name):
+    with open(localization + database_name) as data:
+        return json.load(data)
+
+def header(text):
+    length = len(text)
+    print('\033[1;36m', '-' * (length + 4))
+    print(F'\033[1;33m   {text}')
+    print('\033[1;36m', '-' * (length + 4), '\033[m')
+
+def login(localization, database):
+    header('Login!')
+    with open(localization + database) as database_arc:
+        database_obj = json.load(database_arc)
+        while True:
+            try:
+                x = False
+                email = str(input('Email: ')).strip()
+                if '@' not in email:
+                    print('Email inválido.')
+                    continue
+                for i, c in enumerate(database_obj):
+                    if c['email'] == email:
+                        index = i
+                        x = True
+                        break
+                if x == True:
+                    break
+            except:
+                continue
+        while True:
+            try:
+                password = str(input('Senha: ')).strip()
+                if database_obj[index]['password'] == password:
+                    break
+                else:
+                    continue
+            except:
+                continue
+        return [database_obj[index]['username'],
+                database_obj[index]['email'],
+                database_obj[index]['password'],
+                database_obj[index]['rank'],
+                database_obj[index]['forca-victories'],
+                database_obj[index]['forca-defeats'],
+                database_obj[index]['forca-games'],
+                database_obj[index]['id']
+        ]
+
+def register(localization, database):
+    header('Registrar!')
+    with open(localization + database) as database_arc:
+        obj_database = json.load(database_arc)
+        while True:
+            try:
+                username = str(input("Nome de usuário: ")).strip()
+                for c in obj_database:
+                    if c['username'] == username:
+                        print('Esse nome de usuário já está sendo utilizado!')
+                        continue
+            except:
+                continue
+            else:
+                break
+        while True:
+            try:
+                re = False
+                email = str(input("Email: ")).strip()
+                if '@' not in email:
+                    print('Email Inválido!')
+                    continue
+                for c in obj_database:
+                    if c['email'] == email:
+                        print('Esse email já está sendo utilizado!')
+                        re = True
+                        break
+                if re == True:
+                    continue
+            except:
+                continue
+            else:
+                break
+        while True:
+            try:
+                password = str(input("Senha: ")).strip()
+                if len(password) < 8:
+                    print("Sua senha deve conter pelo menos 8 caracteres!")
+                    continue
+            except:
+                continue
+            else:
+                break
+        dados = {'username': username,
+                 'email': email,
+                 'password': password,
+                 'rank': 'normal',
+                 'forca-victories': 0,
+                 'forca-defeats': 0,
+                 'forca-games': 0,
+                 'id': generate_id(localization, database)
+        }
+        obj_database.append(dados)
+        with open(localization + database, 'w') as data:
+            json.dump(obj_database, data, indent=4)
+    print('Parabéns!!! Você se cadastrou!!!')
+
+def generate_id(localization, database):
+    while True:
+        re = False
+        user_id = [None, None, None, None]
+        for c in range(4):
+            user_id[c] = random.choice(string.ascii_uppercase + string.digits)
+        with open(localization + database) as database_arc:
+            database_obj = json.load(database_arc)
+            for l in database_obj:
+                if l['id'] == user_id:
+                    re = True
+                    break
+            if re == True:
+                continue
+        str_user_id = str()
+        for c in range(4):
+            str_user_id += user_id[c]
+        return str_user_id
+
+def get_index_by_id(localization, database, user_id):
+    with open(localization + database) as database_arc:
+        database_obj = json.load(database_arc)
+        for i, c in enumerate(database_obj):
+            if c['id'] == user_id:
+                return i
+
+def forca(localization, database, index):
+    victories, defeats = forca.jogo()
+    tot_games = victories + defeats
+    with open(localization + database) as database_arc:
+        database_obj = json.load(database_arc)
+        database_obj[index]['forca-victories'] += victories
+        database_obj[index]['forca-defeats'] += defeats
+        database_obj[index]['forca-games'] += tot_games
+        with open(localization + database, 'w') as database_arc2:
+            json.dump(database_obj, database_arc2, indent=4)
+
+def info(username, email, password, rank, victories, defeats, tot_games, id):
+    header('Informações da conta')
+    print(F'''
+---------------------------------------
+ Nome de Usuário: {username}
+ Email: {email}
+ Senha: {'*' * len(password)}
+ Rank: {rank}
+ Id: {id}
+
+ Forca:
+    Vitórias: {victories}
+    Derrotas: {defeats}
+    Total de jogos: {tot_games}
+---------------------------------------
+
+''')
